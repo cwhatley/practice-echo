a7d9d776ffb5c38d765b1f61ec2ad3e4d4636cd7
Chris Whatley <whatleyc@gmail.com>
2015-04-14 00:04:53 -0400

    Checkpoint.

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 'use strict';
 
+var connect = require('connect');
+var serveStatic = require('serve-static');
+var path = require('path');
+var dir = path.resolve(__dirname, './public/');
+console.log('root is', dir);
+connect().use(serveStatic(dir)).listen(process.env.PORT || 8080);
